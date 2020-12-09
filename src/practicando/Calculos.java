05e8f70b9a6509ec8e60b99d2ed5b3d78dfd6372
cesargutierrezpoo <>
2020-12-08 21:59:01 -0600

    primer commit para proyecto grupal 3

---
+++ src/practicando/Calculos.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practicando;
+
+/**
+ *
+ * @author Victor Berrios
+ */
+public class Calculos {
+    
+    public double calcularTotal(int cant, double precioUnit){
+        return cant * precioUnit;
+    }
+    
+    public double calcularImpuesto(double subtotalSuma){
+        return subtotalSuma * 0.15;
+    }
+    
+    public double calcultarTotalPago(double subtotalSuma, double impuesto){
+        return subtotalSuma + impuesto;
+    }
+    
+}
